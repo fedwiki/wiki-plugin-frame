fedwiki/wiki-plugin-frame

commit 5ead2f94909b3376ea504d007ef563877943e818

fix whitespace and move some variables closer to where they are used

client/frame.js
@@ -232,12 +232,12 @@
     // events from iframes don't have an opener
     // ensure that the iframe is one of ours
     if (event.source.opener ||
-        !$(".item.frame iframe").filter((i,el) => el.contentWindow === event.source)) { 
+        !$(".item.frame iframe").filter((i,el) => el.contentWindow === event.source)) {
       if (wiki.debug) {console.log('frameListener - not for us', {event})}
       return
     }
     if (wiki.debug) {console.log('frameListener - ours', {event})}
-    
+
     const {data} = event;
     const {action, keepLineup=false, pageKey=null, page=null, pages={},
            title=null, site=null} = data;
@@ -253,7 +253,6 @@
       $page = $iframe.parents('.page')
     }
 
-    let {name, topic, sourceData} = data
     switch (action) {
     case "sendFrameContext":
       event.source.postMessage({
@@ -287,6 +286,7 @@
       triggerThumb($item, thumb)
       break
     case "publishSourceData":
+      let {name, topic, sourceData} = data
       // TODO change name to topic
       publishSourceData($item, name, sourceData)
       break

commit c7e0087f08c04427e701cf53c946974c445809c4

fix #2: bug introduced in 5ead2f9
there was a reason the variable declaration was outside the switch

client/frame.js
@@ -261,6 +261,7 @@
       $page = $iframe.parents('.page')
     }
 
+    let {name, topic, sourceData} = data
     switch (action) {
     case "sendFrameContext":
       event.source.postMessage({
@@ -294,7 +295,6 @@
       triggerThumb($item, thumb)
       break
     case "publishSourceData":
-      let {name, topic, sourceData} = data
       if (!!name && !topic) {
         const [url='unknown URL'] = $item.data()?.item?.text?.split(/\n/)
         console.warn(`a frame at ${url} sent a "publishSourceData" action with a "name".